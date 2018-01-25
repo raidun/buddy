d6e3d6dc73c577008b84f873ddf37129b8e380b0
Rashid Aidun <raidun@yahoo.com>
2018-01-25 13:22:26 -0800

    working 1st snapshot

---
+++ src/main/java/com/buddy/controllers/HellowWorldController.java
@@ -0,0 +1,13 @@
+package com.buddy.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+class HellowWorldController {
+	
+	@RequestMapping("/")
+   public String sayHello() {
+	   return "index";
+   }
+}
